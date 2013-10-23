f13eb5b3cb470bf98bc6c35bfa488015490ce415
Sasha Matijasic <sasha@selectnull.com>
2013-10-23 18:52:45 +0200

    Look at ~/.venvs for additional environments

--- ave.sh
+++ ave.sh
@@ -2,8 +2,8 @@
 
 function ave {
     activate_script=bin/activate
+    venvs_dir=~/.venvs
     current_dir=$1
-    #echo $current_dir
 
     # if ave is called without arguments, then assume current directory
     if [ -z $current_dir ]
@@ -15,6 +15,10 @@ function ave {
     then
         # if bin/activate exists, source it
         source $current_dir/$activate_script
+    elif [ -e $venvs_dir/`basename $current_dir`/$activate_script ]
+    then
+        # if there is virtual environment inside ~/venvs_dir, source it
+        source $venvs_dir/`basename $current_dir`/$activate_script 
     else
         parent_dir=${current_dir%/*}
         # stop if parent directory is not owned by me
