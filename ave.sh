8717d2b527e01965900b2ce0464d58fc947bd9ab
Sasha Matijasic <sasha@selectnull.com>
2024-02-06 11:12:49 +0100

    Activate the venv in current dir if it has .venv dir

--- ave.sh
+++ ave.sh
@@ -16,14 +16,17 @@ ave() {
         current_dir=$(pwd)
     fi
 
-    if [ -e "$current_dir/$activate_script" ]
-    then
+    if [ -e "$current_dir/$activate_script" ]; then
         # if bin/activate exists, source it
         . "$current_dir/$activate_script"
         # cd to project directory if it's source controled
         [[ -d "$current_dir/.git" ]] || [[ -d "$current_dir/.hg" ]] || [[ -d "$current_dir/.svn" ]] && cd "$current_dir"
-    elif [ -e "$venvs_dir/$(basename "$current_dir")/$activate_script" ]
-    then
+    elif [ -e "$current_dir/.venv/$activate_script" ]; then
+        # check if there is .venv directory under current_dir
+        # and activate it
+        . "$current_dir/.venv/$activate_script"
+        [[ -d "$current_dir/.git" ]] || [[ -d "$current_dir/.hg" ]] || [[ -d "$current_dir/.svn" ]] && cd "$current_dir"
+    elif [ -e "$venvs_dir/$(basename "$current_dir")/$activate_script" ]; then
         # if there is virtual environment inside WORKON_HOME, source it
         . "$venvs_dir/$(basename "$current_dir")/$activate_script"
         # cd to project directory if it's source controled
