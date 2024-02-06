b83610f0cbeb21f65f95d403a1eecb02e3dd9166
Sasha Matijasic <sasha@selectnull.com>
2024-02-06 11:06:13 +0100

    source is a bashism, use . which is POSIX

--- ave.sh
+++ ave.sh
@@ -19,13 +19,13 @@ ave() {
     if [ -e "$current_dir/$activate_script" ]
     then
         # if bin/activate exists, source it
-        source "$current_dir/$activate_script"
+        . "$current_dir/$activate_script"
         # cd to project directory if it's source controled
         [[ -d "$current_dir/.git" ]] || [[ -d "$current_dir/.hg" ]] || [[ -d "$current_dir/.svn" ]] && cd "$current_dir"
     elif [ -e "$venvs_dir/$(basename "$current_dir")/$activate_script" ]
     then
         # if there is virtual environment inside WORKON_HOME, source it
-        source "$venvs_dir/$(basename "$current_dir")/$activate_script"
+        . "$venvs_dir/$(basename "$current_dir")/$activate_script"
         # cd to project directory if it's source controled
         [[ -d "$current_dir/.git" ]] || [[ -d "$current_dir/.hg" ]] || [[ -d "$current_dir/.svn" ]] && cd "$current_dir"
     else
